754811778cc49c97ae73d3bad594ab62e203c7ef
kkkzbh <kkkzbh@vip.qq.com>
2026-09-13 23:34:20 +0800

    fix(release): bound supported CLion builds to verified APIs

--- build.gradle.kts
+++ build.gradle.kts
@@ -151,6 +151,7 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
+            untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
     }
 
