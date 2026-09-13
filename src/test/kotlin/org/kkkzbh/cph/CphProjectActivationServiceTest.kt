3482cbbf925a31b2925f75192eaf2d7761be4769
kkkzbh <kkkzbh@vip.qq.com>
2026-09-13 23:44:15 +0800

    test(state): isolate activation state restoration

--- src/test/kotlin/org/kkkzbh/cph/CphProjectActivationServiceTest.kt
+++ src/test/kotlin/org/kkkzbh/cph/CphProjectActivationServiceTest.kt
@@ -1,17 +1,31 @@
 package org.kkkzbh.cph
 
-import com.intellij.configurationStore.StoreUtil
+import com.intellij.codeInsight.CodeInsightSettings
 import com.intellij.ide.util.PropertiesComponent
-import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.testFramework.HeavyPlatformTestCase
 
-class CphProjectActivationServiceTest : BasePlatformTestCase() {
-    fun testProjectActivationSurvivesSettingsSaveAndServiceRecreation() {
+class CphProjectActivationServiceTest : HeavyPlatformTestCase() {
+    override fun getOpenProjectOptions() = super.getOpenProjectOptions().runPostStartUpActivities(false)
+
+    override fun setUp() {
+        super.setUp()
+        setDefaultCodeInsightSettings(CodeInsightSettings.getInstance().clone())
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun testProjectActivationSurvivesStateRestorationAndServiceRecreation() {
         val properties = PropertiesComponent.getInstance(project)
         val activation = CphProjectActivationService(properties)
+        val storage = properties as PersistentStateComponent<Any>
+        val initialState = requireNotNull(storage.state)
 
         assertFalse(activation.isEnabled())
         activation.enable()
-        StoreUtil.saveSettings(project, true)
+        val savedState = requireNotNull(storage.state)
+        storage.loadState(initialState)
+        assertFalse(CphProjectActivationService(properties).isEnabled())
+        storage.loadState(savedState)
 
         assertTrue(CphProjectActivationService(properties).isEnabled())
     }
